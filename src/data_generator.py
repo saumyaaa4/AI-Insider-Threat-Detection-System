db404318f28c357843f79937908c3c4ada08c9ae
Saumya Bhagat <154803524+saumyaaa4@users.noreply.github.com>
2026-02-14 19:47:10 +0530

    Update data_generator.py

--- src/data_generator.py
+++ src/data_generator.py
@@ -1,1 +1,24 @@
+import pandas as pd
+import numpy as np
+
+def generate_employee_data(num_records=500):
+
+    np.random.seed(42)
+
+    data = {
+        "employee_id": np.random.randint(1000, 1100, num_records),
+        "login_hour": np.random.randint(0, 24, num_records),
+        "files_accessed": np.random.randint(1, 50, num_records),
+        "data_downloaded_mb": np.random.randint(10, 500, num_records),
+        "usb_usage": np.random.randint(0, 2, num_records)
+    }
+
+    df = pd.DataFrame(data)
+
+    return df
+
+
+if __name__ == "__main__":
+    df = generate_employee_data()
+    print(df.head())
 
